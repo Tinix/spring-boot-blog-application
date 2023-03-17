3d862583ad97e89a3da34f6c98ef464a0c374447
Daniel Tinivella <dtinivella@gmail.com>
2023-03-17 18:26:23 -0300

    initial commit

---
+++ src/main/java/com/example/springbootblogapplication2/SpringBootBlogApplication2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogapplication2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogApplication2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogApplication2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootblogapplication2/SpringBootBlogApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootblogapplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
